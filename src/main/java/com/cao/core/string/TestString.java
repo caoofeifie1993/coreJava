b249f0b196b1c1189190b6a45a3b5da1d6b269d3
caofeifei <2511699195@qq.com>
2018-06-13 23:19:48 +0800

    加包

---
+++ src/main/java/com/cao/core/string/TestString.java
@@ -0,0 +1,11 @@
+package com.cao.core.string;
+
+import java.nio.charset.Charset;
+
+public class TestString {
+    public static void main(String[] args) {
+
+        byte [] aa = {81, 82, 83};
+        System.out.println(new String(aa, 1, 2, Charset.defaultCharset()));
+    }
+}
