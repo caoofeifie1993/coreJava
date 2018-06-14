4e52af8b466823da8e1875f0ea13ec0f2d59a590
caoff <2511699195@qq.com>
2018-06-14 10:21:00 +0800

    1

--- src/main/java/com/cao/core/string/TestString.java
+++ src/main/java/com/cao/core/string/TestString.java
@@ -5,6 +5,8 @@ import java.nio.charset.Charset;
 public class TestString {
     public static void main(String[] args) {
 
+        System.out.println("");
+
         byte [] aa = {81, 82, 83};
         System.out.println(new String(aa, 1, 2, Charset.defaultCharset()));
     }
